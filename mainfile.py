2ea4f0adacac18469ae867c0911ef20e4eb60c3f
Okogbule Goodluck <okogbulegoodluck@gmail.com>
2024-11-14 10:38:08 +0100

    update remaining code

--- mainfile.py
+++ mainfile.py
@@ -238,13 +238,58 @@ for epoch in range(EPOCHS):
                 # using teacher forcing 
                 dec_input = tf.expand_dims(targ[:, t], 1)
 
-            batch_loss = (loss / int(targ.shape[1]))
+        batch_loss = (loss / int(targ.shape[1]))
 
-            total_loss += batch_loss
+        total_loss += batch_loss
 
-            variables = encoder.variables + decoder.variables
+        variables = encoder.variables + decoder.variables
 
-            gradients = tape.gradient(loss, variables)
+        gradients = tape.gradient(loss, variables)
 
-            optimizer.apply_gradient(zip(gradients,variables))
+        optimizer.apply_gradient(zip(gradients,variables))
 
+        if batch % 100 == 0:
+            print('Epoch {} Batch {} Loss {:.4f}'.format(epoch +1, batch, batch_loss.numpy()))
+
+            # saving (checkpoint) the model every 2 seconds
+    if (epoch +1) % 2 ==0:
+        checkpoint.save(file_prefix = checkpoint_prefix)
+    print('Epoch {} Loss {:.4f}'.format(epoch + 1, total_loss/N_BATCH))
+    print('Time taken for 1 epoch {} sec\n'.format(time.time()-start))
+
+
+    def evaluate(sentence, encoder, decoder, inp_lang, targ_lang, max_length_inp, max_length_targ):
+        attention_plot = np.zeros((max_length_targ, max_length_inp))
+        sentence = preprocess_sentence(sentence)
+
+        inputs= [inp_lang.word2idx[i] for i in sentence.split(' ')]
+        inputs= tf.keras.preprocessing.sequence.pad_sequences([inputs], maxlen=max_length_inp, padding='post')
+        inputs= tf.convert_to_tensor(inputs)
+
+        results = ''
+
+        hidden = [tf.zeros((1, units))]
+        enc_out, enc_hidden = encoder(inputs, hidden)
+        dec_hidden = enc_hidden
+        dec_input = tf.expand_dims([targ_lang.word2dix['<start>']], 0)
+
+        for t in range(max_length_targ):
+            predictions, dec_hidden, attention_weights = decoder(dec_input, dec_hidden, enc_out)
+
+            # storing the attention weigths to plot later on
+            attention_weights - tf.reshape(attention_weights, (-1, ))
+            attention_plot[t] = attention_weights.numpy()
+
+        predicted_id = tf.argmax(predictions[0]).numpy()
+        result += targ_lang.idx2word[predicted_id] + ' '
+
+        if targ_lang.idx2word[predicted_id] == '<end>':
+            return result, sentence, attention_plot
+        
+        # the predicted ID is fed back into the model 
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+        return result, sentence, attention_plot
+    
+# restoring the latest checkpoint in checkpoint_dir 
+checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
